34e3b10d0c74674d4ce54a8adc14750282516534
João Victor <106630200+SrJohn369@users.noreply.github.com>
2023-09-17 17:55:11 -0300

    estruturando

---
+++ fastAPI/back_end/fastAPI_BancoDados/src/schema/schemas.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel
+from typing import Optional, List
+
+
+class Usuario(BaseModel):
+    id: Optional[str] = None
+    nome: str
+    telefone: str
+    minhas_vendas: List[Pedido]
+    meus_pedidos: List[Pedido]
+    meus_produtos: List[Produto]
+
+    
+class Produto(BaseModel):
+    id: Optional[str] = None
+    nome: str
+    detalhes: str
+    preco: float
+    disponivel: bool
+    
+
+class Pedido(BaseModel):
+    id: Optional[str] = None
+    usuario: Usuario
+    produto: Produto
+    quantidade: int
+    entrga: bool = True
+    endereco: str
+    observacoes: Optional[str] = "Sem observações"
+
+
+if __name__ == '__main__':
+    pass
